8fbed81a8af7970bf2263e2751bf4e0755590b0c
Windy <windy@Windy-MacbookPro.local>
2021-03-18 17:07:03 +0800

    Align digital pin with 315MHz board, make ON/OFF/STOP usable as described

--- init.lua
+++ init.lua
@@ -1,59 +1,66 @@
---function printclient()
---  for mac,ip in pairs(wifi.ap.getclient()) do
---  print(mac,ip)
---end
-
 do
-wifi.setmode(wifi.SOFTAP)
-wifi.ap.config({ ssid = "WEB control LED", pwd = "12345678" })
---wifi.event.register(wifi.eventmon.AP_STACONNECTED,printclient())
+  wifi.setmode(wifi.SOFTAP)
+  wifi.ap.config({ ssid = "WEB control LED", pwd = "12345678" })
 
-gpio.mode(0, gpio.OUTPUT)
-local srv = net.createServer(net.TCP)
-srv:listen(80, function(conn)
-  conn:on("receive", function(client, request)
-    local buf = ""
-    local _, _, method, path, vars = string.find(request, "([A-Z]+) (.+)?(.+) HTTP")  -- luacheck: no unused
-    if (method == nil) then
-      _, _, method, path = string.find(request, "([A-Z]+) (.+) HTTP")  -- luacheck: no unused
-    end
+  onpin = 4
+  offpin = 8
+  stoppin = 9
+  idlepin = 3
+  powerpin = 10
 
-    local _GET = {}
-    if (vars ~= nil) then
-      for k, v in string.gmatch(vars, "(%w+)=(%w+)&*") do
-        _GET[k] = v
-        print ("k: "..k)
-        print ("v: "..v)
-      end
-    end
-    buf = buf .. "<!DOCTYPE html><html><body><h1>Hello, this is NodeMCU.</h1>"
-    .. "<form src=\"/\">Turn PIN1 <button name=\"act\" value=\"ON\" onclick=\"form.submit()\"> On"
-    .. "<button name=\"act\" value=\"OFF\" onclick=\"form.submit()\"> Off"
-    .. "<button name=\"act\" value=\"STOP\" onclick=\"form.submit()\"> STOP!"
+  function initpin(pin)
+    gpio.mode(pin,gpio.OUTPUT)
+    gpio.write(pin,gpio.LOW)
+  end
 
-    if (_GET.act == "ON") then
-      print("turning LED on")
-      gpio.write(0, gpio.LOW)
-    elseif (_GET.act == "OFF") then
-      print("turn LED off")
-      gpio.write(0, gpio.HIGH)
-    elseif (_GET.act == "STOP") then
-      print("blinking LED")
+  for pin = 3,10,1 do
+    initpin(pin)
+  end
+
+  function sendpin (datapin, powerpin)
+      gpio.mode(datapin, gpio.OUTPUT)
+      gpio.mode(powerpin, gpio.OUTPUT)
       tmr.create():alarm(1, tmr.ALARM_SINGLE, function()
-        gpio.write(0, gpio.LOW)
-        tmr.delay(1000000)
-        gpio.write(0, gpio.HIGH)
-        tmr.delay(1000000)
-        gpio.write(0, gpio.LOW)
-        tmr.delay(1000000)
-        gpio.write(0, gpio.HIGH)
-        tmr.delay(1000000)
-        gpio.write(0, gpio.LOW)
+          gpio.write(powerpin, gpio.HIGH)
+          print("read powerpin: "..gpio.read(powerpin))
+          gpio.write(datapin, gpio.HIGH)
+          print("read datapin: "..gpio.read(datapin))
+          tmr.delay(300000)
+          gpio.write(powerpin, gpio.LOW)
+          print("read powerpin: "..gpio.read(powerpin))
+          gpio.write(datapin, gpio.LOW)
+          print("read datapin: "..gpio.read(datapin))
       end)
-    end
-    buf = buf .. "</button> </form></body></html>"
-    client:send(buf)
+  end
+  
+  local srv = net.createServer(net.TCP)
+  srv:listen(80, function(conn)
+    conn:on("receive", function(client, request)
+      local buf = ""
+      local _, _, method, path, vars = string.find(request, "([A-Z]+) (.+)?(.+) HTTP")  -- luacheck: no unused
+      if (method == nil) then
+        _, _, method, path = string.find(request, "([A-Z]+) (.+) HTTP")  -- luacheck: no unused
+      end
+      local _GET = {}
+      if (vars ~= nil) then
+        for k, v in string.gmatch(vars, "(%w+)=(%w+)&*") do
+          _GET[k] = v
+        end
+      end
+      buf = buf .. "<!DOCTYPE html><html><body><h1>Hello, this is NodeMCU.</h1>"
+      .. "<form src=\"/\">Turn PIN1 <button name=\"act\" value=\"ON\" onclick=\"form.submit()\"> On"
+      .. "<button name=\"act\" value=\"OFF\" onclick=\"form.submit()\"> Off"
+      .. "<button name=\"act\" value=\"STOP\" onclick=\"form.submit()\"> STOP!"
+      if (_GET.act == "ON") then
+        sendpin (onpin,powerpin)
+      elseif (_GET.act == "OFF") then
+        sendpin (offpin,powerpin)
+      elseif (_GET.act == "STOP") then
+        sendpin (stoppin,powerpin)
+      end
+      buf = buf .. "</button> </form></body></html>"
+      client:send(buf)
+    end)
+    conn:on("sent", function(c) c:close() end)
   end)
-  conn:on("sent", function(c) c:close() end)
-end)
 end
